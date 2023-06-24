6d20a323a7a97e38abcfc59557820ec6afe57dc5
Marc Mikhail <Mrcmikhail@gmail.com>
2023-06-23 22:51:25 -0400

    day 1 advent of code

---
+++ day_1/caloriecount1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main () {
+    file, err := os.Open("input.txt")
+
+    if err != nil {
+        fmt.Printf("Failed to open the CSV file: %s", "input.csv")
+        panic(0)
+    }
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+
+    temp := 0
+    max := 0
+
+    for {
+        line, err := reader.ReadString('\n')
+        if err != nil { break }
+        if line == "\n" {
+            if temp > max{
+                max = temp
+            }
+            temp = 0
+        }
+        calories, err := strconv.Atoi(strings.TrimSpace(line))
+        temp += calories
+    }
+
+    fmt.Println(max)
+
+}
+
